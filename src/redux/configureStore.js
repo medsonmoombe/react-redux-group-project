0e234572f17834694d0d5f805349279a453dfec4
medsonmoombe <medsonmoombe21@gmail.com>
2022-06-22 15:02:44 +0200

    Resolved conflicts

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,27 +1,27 @@
-<<<<<<< HEAD
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import missionReducer from './missions/MissionSlice';
+import { applyMiddleware, compose } from 'redux';
+import rocketReducer from './rockets/rockets';
+import thunk from 'redux-thunk';
 
 const rootReducers = combineReducers({
   missions: missionReducer,
+  rockets: rocketReducer,
 });
 
 const store = configureStore({
   reducer: rootReducers,
 });
 export default store;
-=======
-import { configureStore } from '@reduxjs/toolkit';
-import { applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import rocketReducer from './rockets/rockets';
+
+
+
 
 const composeEnhancers = compose;
 
-export default configureStore({
-  reducer: {
-    rockets: rocketReducer,
-  },
-},
-composeEnhancers(applyMiddleware(thunk)));
->>>>>>> dev
+// export default configureStore({
+//   reducer: {
+//     rockets: rocketReducer,
+//   },
+// },
+composeEnhancers(applyMiddleware(thunk));
