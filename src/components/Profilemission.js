b4d7bfe5077b71087738ffe55791155e71b9d87e
medsonmoombe <medsonmoombe21@gmail.com>
2022-06-23 11:35:01 +0200

    resolve conflicts

--- src/components/Profilemission.js
+++ src/components/Profilemission.js
@@ -2,21 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Profilemission = ({ reservedMission }) => {
-    <>
-      <li>
-        {
+  <>
+    <li>
+      {
         reservedMission.map((mission) => (
           <li className="joined-mission" key={mission.mission_id}>
             <span>{mission.mission_name}</span>
           </li>
         ))
       }
-      </li>
-    </>;
+    </li>
+  </>;
 };
 
 Profilemission.propTypes = {
   reservedMission: PropTypes
-    .arrayOf(PropTypes.oneOfType([PropTypes.number, PropTypes.string])).isRequired,
+    .arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number])).isRequired,
 };
 export default Profilemission;
