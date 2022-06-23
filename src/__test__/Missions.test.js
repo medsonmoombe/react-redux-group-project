013d20dbcd7c766fd12bf007a059843f24193178
medsonmoombe <medsonmoombe21@gmail.com>
2022-06-23 14:25:59 +0200

    Add the test folder

--- src/__test__/Missions.test.js
+++ src/__test__/Missions.test.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
-export const Missions.test = () => {
-  return (
-    <div>Missions.test</div>
-  )
-}
+const Missions = () => (
+  <div>Missions.test</div>
+);
